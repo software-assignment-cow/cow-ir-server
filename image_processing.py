fa3b7b0136c055554770c04bc7bef944284dc9b8
경민 <rlaru@naver.com>
2026-05-22 19:02:30 +0900

    feat: 이미지 처리 및 DB 비즈니스 로직 파일 구조 분리 완료

---
+++ image_processing.py
@@ -0,0 +1,12 @@
+# image_processing.py (임시 뼈대)
+
+def calculate_similarity(uploaded_image_bytes, registered_image_bytes):
+    """
+    [B, C 담당 영역] 
+    나중에 팀원들이 완성한 OpenCV ORB + BFMatcher 로직이 
+    이 함수 안으로 들어올 예정입니다!
+    """
+    print("OpenCV 이미지 비교 엔진 가동 중...")
+    
+    # 지금은 일단 임시로 99.9점을 반환하게 해둡니다 (가짜 함수)
+    return 99.9
